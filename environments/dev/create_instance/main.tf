53dccfae4dbf1e2c8ac8168956abdd26de5447d8
Dante Beltran <dante.beltran.ext@boehringer-ingelheim.com>
2025-09-28 23:20:40 -0300

    rewriten source paths

--- environments/dev/create_instance/main.tf
+++ environments/dev/create_instance/main.tf
@@ -2,13 +2,13 @@
 
 # 1. Definición del ECR
 module "ecr" {
-  source   = "../../modules/ecr"
+  source   = "../../../modules/ecr"
   app_name = var.app_name 
 }
 
 # 2. Definición del Servidor (EC2, EIP, SG)
 module "compute_server" {
-  source          = "../../modules/compute"
+  source          = "../../../modules/compute"
   app_name        = var.app_name
   instance_type   = var.instance_type
 }
