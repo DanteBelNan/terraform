cf7b1221e451d46481bc97cfa24bb92e92f615e8
Dante Beltran <dante.beltran.ext@boehringer-ingelheim.com>
2025-10-02 09:08:49 -0300

    added force delete on deleting images

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -3,6 +3,7 @@ resource "aws_ecr_repository" "app_repos" {
   
   name                 = "${lower(var.app_name)}-${each.value}"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
